99d79ffc99ec1fbfe4ca71a98d4e655d9e743ac6
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-07-10 19:37:34 -0300

    Added function for operators and equal

--- main.js
+++ main.js
@@ -14,24 +14,34 @@ function divide(a, b) {
   return a / b;
 }
 
-let num1;
-let operator;
-let num2;
+function remainder(a, b) {
+  return a % b;
+}
+
+let num1 = "";
+let operator = "";
+let num2 = "";
 
 function operate(opr, a, b) {
   switch (opr) {
     case "+":
-      add(a, b);
+      return add(a, b);
     case "-":
-      substract(a, b);
+      return substract(a, b);
     case "x":
-      multiply(a, b);
+      return multiply(a, b);
     case "/":
-      divide(a, b);
+      return divide(a, b);
+    case "%":
+      return remainder(a, b);
   }
 }
 
 let numberBtn = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+console.log(operators);
+let equal = document.querySelector("#equal");
+let zero = document.querySelector("#zero");
 let display = document.querySelector("#display");
 
 numberBtn.forEach((btn) => {
@@ -39,4 +49,31 @@ numberBtn.forEach((btn) => {
     let value = btn.innerText;
     display.textContent += value;
   });
+});
+
+operators.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("clickeo");
+    if (operator === "") {
+      num1 = Number(display.innerText);
+      console.log("num1 antes:", num1);
+      operator = btn.innerText;
+      console.log("operator:", btn.innerText);
+      display.innerText = "";
+      console.log("display:", display.innerText);
+    }
+  });
+});
+
+equal.addEventListener("click", () => {
+  console.log("click equal");
+  if (display.innerText !== "") {
+    num2 = Number(display.innerText);
+    console.log(num2);
+    console.log(`el num1 es ${num1}`);
+    console.log(`el operador es ${operator}`);
+    result = operate(operator, num1, num2);
+    console.log(result);
+    display.innerText = result;
+  }
 });
