b2f78902cf95da672ac0bde73f92d52fd3acd2bf
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-07-11 02:20:32 -0300

    Operations with integers logic finished

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  if (b === 0) {
+    return "Imposible dvidir por zero";
+  } else {
+    return a / b;
+  }
 }
 
 function remainder(a, b) {
   return a % b;
 }
-
-let num1 = "";
-let operator = "";
-let num2 = "";
 
 function operate(opr, a, b) {
   switch (opr) {
@@ -42,38 +42,60 @@ let operators = document.querySelectorAll(".operator");
 console.log(operators);
 let equal = document.querySelector("#equal");
 let zero = document.querySelector("#zero");
+let backspace = document.querySelector("#backspace");
 let display = document.querySelector("#display");
+let operatorsValues = [];
+for (let values of operators) {
+  operatorsValues.push(values.innerText);
+}
+console.log(operatorsValues);
 
 numberBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     let value = btn.innerText;
-    display.textContent += value;
+    display.innerText += value;
   });
 });
 
 operators.forEach((btn) => {
   btn.addEventListener("click", () => {
     console.log("clickeo");
-    if (operator === "") {
-      num1 = Number(display.innerText);
-      console.log("num1 antes:", num1);
-      operator = btn.innerText;
-      console.log("operator:", btn.innerText);
-      display.innerText = "";
-      console.log("display:", display.innerText);
+    if (!operatorsValues.some((value) => display.innerText.includes(value))) {
+      display.innerText += btn.textContent;
     }
   });
 });
 
 equal.addEventListener("click", () => {
+  let operator = operatorsValues.find((value) =>
+    display.innerText.includes(value),
+  );
+  console.log(operator);
   console.log("click equal");
-  if (display.innerText !== "") {
-    num2 = Number(display.innerText);
-    console.log(num2);
-    console.log(`el num1 es ${num1}`);
-    console.log(`el operador es ${operator}`);
+  let num1extract = display.innerText.slice(
+    0,
+    display.innerText.indexOf(`${operator}`),
+  );
+  let num2extract = display.innerText.slice(
+    display.innerText.indexOf(`${operator}` + 1),
+  );
+  num1 = num1extract === "" ? Nan : Number(num1extract);
+  num2 = num2extract === "" ? Nan : Number(num2extract);
+  console.log(`el operador es ${operator}`);
+  console.log(`num1 es ${num1}`);
+  console.log(`num2 es ${num2}`);
+  if (operator !== "" && !isNaN(num2)) {
     result = operate(operator, num1, num2);
     console.log(result);
     display.innerText = result;
+    operator = "";
   }
+});
+
+zero.addEventListener("click", () => {
+  display.innerText = "";
+});
+
+backspace.addEventListener("click", () => {
+  display.innerText = display.innerText.slice(0, display.innerText.length - 1);
 });
